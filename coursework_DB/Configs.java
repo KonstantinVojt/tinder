47ae4d5477a7f56c1c69848482909a0ab052c3b9
KonstantinVojt <114500793+KonstantinVojt@users.noreply.github.com>
2023-12-10 23:32:14 +0300

    Add files via upload

---
+++ coursework_DB/Configs.java
@@ -0,0 +1,9 @@
+package coursework_DB;
+
+public class Configs {
+    protected String dbHost = "localhost";
+    protected String dbPort = "5432";
+    protected String dbUser = "postgres";
+    protected String dbPass = "12kOsta71";
+    protected String dbName = "Eco-productsStore";
+}
