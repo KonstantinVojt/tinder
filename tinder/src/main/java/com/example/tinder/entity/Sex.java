df34af20e630df930a4068563e5bdbfa2d09b5de
KonstantinVojt <114500793+KonstantinVojt@users.noreply.github.com>
2024-02-08 15:01:04 +0300

    Delete tinder directory

--- tinder/src/main/java/com/example/tinder/entity/Sex.java
+++
@@ -1,6 +0,0 @@
-package com.example.tinder.entity;
-
-public enum Sex {
-    MALE,
-    FEMALE
-}
--- tinder/src/main/java/com/example/tinder/service/match/MatchService.java
+++
@@ -1,9 +0,0 @@
-package com.example.tinder.service.match;
-
-import com.example.tinder.entity.User;
-
-public interface MatchService {
-
-    User getNewMatch();
-
-}
