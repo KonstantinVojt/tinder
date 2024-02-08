761c361016087c0d1b636583ddeb4b20d99962fd
KonstantinVojt <114500793+KonstantinVojt@users.noreply.github.com>
2024-02-08 14:54:33 +0300

    Delete coursework_DB directory

--- coursework_DB/Configs.java
+++
@@ -1,9 +0,0 @@
-package coursework_DB;
-
-public class Configs {
-    protected String dbHost = "localhost";
-    protected String dbPort = "5432";
-    protected String dbUser = "postgres";
-    protected String dbPass = "12kOsta71";
-    protected String dbName = "Eco-productsStore";
-}
